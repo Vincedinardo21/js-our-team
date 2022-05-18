77d563811c07b1bfcd5ae40150361522e531cf35
vince <vincedinardo21@gmail.com>
2022-05-18 17:51:10 +0200

    Tentativo di ciclo (fallito)

--- myscript.js
+++ myscript.js
@@ -1,17 +1,57 @@
 // Variabili
 let myContainer = document.querySelector(".team-container");
-let content = document.createElement("h3");
 
+// const card = {
+//     "immagine": ["img/wayne-barnett-founder-ceo.jpg", "img/angela-caroll-chief-editor.jpg", "img/wayne-barnett-founder-ceo.jpg", "img/wayne-barnett-founder-ceo.jpg", "img/wayne-barnett-founder-ceo.jpg", "img/wayne-barnett-founder-ceo.jpg"],
+//     "nome": ["wayne", "angela", "walter", "lopez", "scott", "barbara"],
+//     "ruolo": ["Chief", "Office", "Social", "Developer", "Graphic"]
+// };
 
-const card = {
-    "immagine": [],
-    "nome": ["wayne", "angela", "walter", "lopez", "scott", "barbara"],
-    "ruolo": []
-};
+// for(let i = 0; i < 5; i++){
+//     let myImage = document.createElement("img");
+//     //myImage.src = "img/wayne-barnett-founder-ceo.jpg";
+//     myImage.append(card.immagine[i]);
+//     myContainer.append(myImage);
+// }
 
-for(let i = 0; i < 5; i++){
-    content.append(card.nome[i]);
-    myContainer.append(content);
+const card = [
+    {
+        "immagine" : "img/angela-caroll-chief-editor.jpg",
+        "nome" : "angela",
+        "ruolo" : "Chef Editor"
+    },
+    {
+        "immagine" : "img/angela-caroll-chief-editor.jpg",
+        "nome" : "angela",
+        "ruolo" : "Chef Editor"
+    },
+    {
+        "immagine" : "img/angela-caroll-chief-editor.jpg",
+        "nome" : "angela",
+        "ruolo" : "Chef Editor"
+    },
+    {
+        "immagine" : "img/angela-caroll-chief-editor.jpg",
+        "nome" : "angela",
+        "ruolo" : "Chef Editor"
+    },
+    {
+        "immagine" : "img/angela-caroll-chief-editor.jpg",
+        "nome" : "angela",
+        "ruolo" : "Chef Editor"
+    },   
+]
+
+for(i = 0; i < card.length; i++){
+    
+    let objIesimo  = card[i];
+    for(var chiave in objIesimo){
+        let myContent = document.createElement("div");
+        myContent.classList.add(".team-card");
+        myContent.append(objIesimo[chiave]);
+        myContainer.append(myContent);
+        console.log(chiave + " : " + objIesimo[chiave]);
+    }
 }
 
 
